b2c6996ebaac15f1d859f44f8569fce317ea9c04
prajakta890 <prajaktabhandvalkar8802@gmail.com>
2023-11-03 09:42:18 +0530

    Modified my file

--- User_Defined_Data_Structure.c
+++ User_Defined_Data_Structure.c
@@ -14,12 +14,12 @@ typedef char *string;
 int main(void)
 {
     new tup[2];
-    string p = "Pratham";
+    string p = "Prajakta";
     tup[0].index = 0;
     tup[0].name = 'P';
 
     tup[1].index = 1;
-    tup[1].name = 'T';
+    tup[1].name = 'B';
 
     printf("\n%c %c\n", tup[0].name,tup[1].name);  
     printf("%s\n",p);
